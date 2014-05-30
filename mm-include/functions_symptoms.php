b8e4a5a66e4226148d0374c462b1bfae54f689b8
Henri Schumacher <henri.hulski@gazeta.pl>
2014-05-30 10:49:04 +0200

    Adding license headers to the scripts of Thomas

--- mm-include/functions_symptoms.php
+++ mm-include/functions_symptoms.php
@@ -1,4 +1,32 @@
 <?php
+
+/**
+ * mm-include/functions_symptoms.php
+ *
+ * Symptom functions that are be used by the symptom-details.php script from Thomas Bochmann
+ *
+ * PHP version 5
+ *
+ * LICENSE: This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Homeopathy
+ * @package   SymptomFunctionsMm
+ * @author    Thomas Bochmann <thomas.bochmann@gmail.com>
+ * @copyright 2009 Thomas Bochmann
+ * @license   http://www.gnu.org/licenses/agpl.html GNU Affero General Public License v3
+ * @version   1.0
+ * @link      http://openhomeo.org/openhomeopath/download/openhomeopath_1.0.tar.gz
+ */
+
 function get_select_all_rubric()
 {
 	global $translations, $lng, $db;
