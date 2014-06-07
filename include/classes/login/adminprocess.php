d06b303d5c88f5b32295ace94c4154d4a118a0de
Henri Schumacher <henri.hulski@gazeta.pl>
2014-06-07 12:57:22 +0200

    Update PHP-Documentation with apigen

--- include/classes/login/adminprocess.php
+++ include/classes/login/adminprocess.php
@@ -46,13 +46,15 @@ class  AdminProcess {
 	/**
 	 * Class constructor
 	 *
-	 * @return AdminProcess
+	 * @return void
 	 * @access public
 	 */
 	function __construct() {
 		global $session;
 		/* Make sure administrator is accessing page */
 		if(!$session->isAdmin()) {
+			$host  = $_SERVER['HTTP_HOST'];
+			$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 			$extra = "login.php";
 			header("Content-Type: text/html;charset=utf-8");
 			header("Location: ../../../$extra");
@@ -228,6 +230,7 @@ class  AdminProcess {
 	 */
 	private function procDeleteInactive() {
 		global $session, $db;
+		$inact_time = $session->time - $_POST['inactdays']*24*60*60;
 		$query = "SELECT username FROM " . TBL_USERS . " WHERE TIMESTAMPDIFF(DAY, `timestamp`, NOW()) > " . $_POST['inactdays'] . " AND userlevel != " . ADMIN_LEVEL;
 		$db->send_query($query);
 		while($username = $db->db_fetch_row()) {
@@ -257,7 +260,7 @@ class  AdminProcess {
 				$count = $db->db_num_rows();
 				$db->free_result();
 				if ($count > 0) {
-					$query = "INSERT INTO " . TBL_BANNED_USERS . " VALUES ('$subuser')";
+					$query = "INSERT INTO " . TBL_BANNED_USERS . " VALUES ('$subuser', $session->time)";
 					$db->send_query($query);
 				}
 				/*
@@ -266,11 +269,11 @@ class  AdminProcess {
 				$user_id = $db->getUserInfo($subuser, 'id_user');
 				$user_ar = array("user_id" => $user_id[0]);
 				embed_phorum_deleteuser($user_ar);
-				$query = "DELETE FROM " . TBL_USERS . " WHERE username = '$subuser;
-				$db->send_query($query);
 			}
 		}
-		header("Content-Type: text/html;charset=utf-8");
+		$query = "DELETE FROM " . TBL_USERS . " WHERE timestamp < $inact_time AND userlevel != " . ADMIN_LEVEL;
+		$db->send_query($query);
+		header("Content-Type: text/html;charset=utf-8"); 
 		header("Location: " . $session->referrer);
 		die();
 	}
@@ -320,7 +323,7 @@ class  AdminProcess {
 			$db->free_result();
 			$query = "DELETE FROM repertorizations WHERE username = '$subuser'";
 			$db->send_query($query);
-			$query = "INSERT INTO " . TBL_BANNED_USERS . " VALUES ('$subuser')";
+			$query = "INSERT INTO " . TBL_BANNED_USERS . " VALUES ('$subuser', $session->time)";
 			$db->send_query($query);
 			/*
 			 * Delete user from phorum user-table.
@@ -382,7 +385,7 @@ class  AdminProcess {
 		$subuser = $_POST["deluserdata"];
 		$field = "deluserdata";  //Use field name for username
 		if(!$subuser || strlen($subuser = trim($subuser)) == 0) {
-			$form->setError($field, "* " . _("Please enter your username") . "<br>");
+			$form->setError($field, "* " . _("Please enter a username") . "<br>");
 		}
 		/*
 		 * Errors exist, have user correct them
@@ -468,7 +471,7 @@ class  AdminProcess {
 		$subuser = $_POST[$user_post];
 		$field = $username;  // Use field name for username
 		if(!$subuser || strlen($subuser = trim($subuser)) == 0) {
-			$form->setError($field, "* " . _("Please enter your username") . "<br>");
+			$form->setError($field, "* " . _("Please enter a username") . "<br>");
 		} else {
 			/*
 			 * Make sure username is in database
